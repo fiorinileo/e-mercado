d978f5b64bd9e2359ae7e9d1044489a4de124844
fiorinileo <leonardofiorini87@gmail.com>
2022-08-16 23:12:19 -0300

    primera version de login

---
+++ js/login.js
@@ -0,0 +1,6 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+document.addEventListener('DOMContentLoader', function (e) {
+  myInput.focus()
+})
