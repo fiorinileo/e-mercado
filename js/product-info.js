ef965c35c9b9800d45edd88214e8dd9a8d4a2a30
fiorinileo <leonardofiorini87@gmail.com>
2022-09-18 20:15:44 -0300

    implementación de contador de articulos del carrito global

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,5 @@
 var date = new Date();
 function drawScore(place, value) {
-  console.log(document.getElementById("generalScore"));
   document.getElementById("generalScore").innerHTML = `
                             Calificación promedio: 
                                 <span class="fa "></span>
@@ -61,8 +60,9 @@ function showProductInfo() {
     product.cost
   }</p>
                             <div class=" col product-cta-btn pe-0">
-                                <button class="hoverAnim-2">
-                                    COMPRAR
+                            <input type=number id="nudCant" min=1 max=100 value="1">
+                            <button id="buy_btn" class="hoverAnim-2">
+                                COMPRAR
                                     <svg class="mb-1" viewBox="0 0 576 512"><path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
                                 </button>
                             </div>
@@ -317,8 +317,31 @@ document.addEventListener("DOMContentLoaded", function (e) {
       .then(function (resultObj) {
         if (resultObj.status === "ok") {
           product = resultObj.data;
+          currentProduct=String(product.id) ;
           showProductInfo();
           showRelatedProducts();
+          document.getElementById("buy_btn").addEventListener("click",()=>{
+            let cart = {};
+            if (localStorage.getItem("cart")) {
+              cart=JSON.parse(localStorage.getItem("cart"));
+              if(cart[currentProduct]){
+                let cant=cart[currentProduct]+parseInt(document.getElementById("nudCant").value);
+                cart[currentProduct]=cant;
+                localStorage.setItem("cart", JSON.stringify(cart));
+              }
+              else{
+                cart[currentProduct]=parseInt(document.getElementById("nudCant").value);
+                localStorage.setItem("cart", JSON.stringify(cart));
+              }
+            }
+            else{
+              cart[currentProduct] =parseInt(document.getElementById("nudCant").value); 
+              localStorage.setItem("cart",JSON.stringify(cart))
+            }
+            
+            showProductInCart();
+            
+          })
         }
       })
       .then(() => {
